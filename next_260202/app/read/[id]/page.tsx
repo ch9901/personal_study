cd9578f06fdfcc53a1b27e8f51d766c286e66699
ch9901 <jcy990113@gmail.com>
2026-02-03 22:57:22 +0900

    Next.js 13-12 완료

--- next_260202/app/read/[id]/page.tsx
+++ next_260202/app/read/[id]/page.tsx
@@ -1,9 +1,12 @@
 export default async function Read(props: any) {
   const { id } = await props.params;
+  const resp = await fetch(`http://localhost:9999/topics/${id}`);
+  const topic = await resp.json();
 
   return (
     <>
-      <h2>READ</h2>
+      <h2>{topic.title}</h2>
+      <p>{topic.body}</p>
       parameter : {id}
     </>
   );
